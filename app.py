0e922e130e8657f7c34b01636028b77b94b16fcf
zhongmenduiju123-art <zhongmenduiju123@gmail.com>
2025-11-25 03:16:11 +0000

    1

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
+    r = request.form.get("p")
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
 
 if __name__=="__main__":
     app.run()
